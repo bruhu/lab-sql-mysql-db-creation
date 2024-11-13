17ed64060362651e508206bb604eedd410c8fb09
bru <brugera.herrero@gmail.com>
2024-11-13 11:14:19 +0100

    bonus

--- seeding.sql
+++ seeding.sql
@@ -36,3 +36,19 @@ SELECT * FROM customers;
 SELECT * FROM salespersons;
 SELECT * FROM invoices;
 
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE customers
+SET email = 'ppicasso@gmail.com'
+WHERE name = 'Pablo Picasso';
+
+UPDATE customers
+SET email = 'lincoln@us.gov'
+WHERE name = 'Abraham Lincoln';
+
+UPDATE customers
+SET email = 'hello@napoleon.me'
+WHERE name = 'Napoléon Bonaparte';
+
+SET SQL_SAFE_UPDATES = 1;
+SELECT * FROM customers;
