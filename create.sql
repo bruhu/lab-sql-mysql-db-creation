6d600a45cabff25386b893106575f40af574446a
bru <brugera.herrero@gmail.com>
2024-11-13 11:11:33 +0100

    insert values in every table

--- create.sql
+++ create.sql
@@ -9,7 +9,7 @@ DROP TABLE IF EXISTS salespersons;
 
 CREATE TABLE cars (VIN VARCHAR(100), manufacturer VARCHAR(100), model VARCHAR(100), year YEAR, color VARCHAR(50), price DECIMAL);
 CREATE TABLE invoices (invoice_number INT, date DATE, car_VIN VARCHAR(17), customer_id INT, staff_ID INT, total_price DECIMAL); 
-CREATE TABLE customers (customer_id INT, name VARCHAR(255), phone_number VARCHAR(15), email VARCHAR(255) NOT NULL, address TEXT, city VARCHAR(100), state_province VARCHAR(100), country VARCHAR(100), zip_code VARCHAR(20));
+CREATE TABLE customers (customer_id INT, name VARCHAR(255), phone_number VARCHAR(20), email VARCHAR(255), address TEXT, city VARCHAR(100), state_province VARCHAR(100), country VARCHAR(100), zip_code VARCHAR(20));
 CREATE TABLE salespersons (staff_id INT, name VARCHAR(100), store VARCHAR(100));
 
 SHOW TABLES;
--- seeding.sql
+++ seeding.sql
@@ -1,18 +1,38 @@
-ALTER TABLE cars MODIFY price DECIMAL(10, 2);
+ALTER TABLE cars MODIFY price DECIMAL(10, 2);sys_configaccount
+ALTER TABLE invoices MODIFY total_price DECIMAL(10, 2);
 
-INSERT INTO cars (VIN, manufacturer, model, year, color, price)
- VALUES ('1HGBH41JXMN109186', 'Toyota', 'Camry', 2022, 'Silver', 24999.99),
- ('2HGFA16556H543234', 'Honda', 'Civic', 2021, 'Red', 21999.99),
- ('3LN6L5F94ER603222', 'Lincoln', 'MKZ', 2023, 'Black', 34999.99),
- ('5NMS33AD7GH213049', 'Hyundai', 'Santa Fe', 2022, 'Blue', 27999.99),
- ('1C4PJMDX8KD500246', 'Jeep', 'Cherokee', 2020, 'White', 25999.99),
- ('4T1BF1FK5HU557890', 'Toyota', 'Corolla', 2021, 'Gray', 20999.99),
- ('1FT8W3BT8JFC72030', 'Ford', 'F-150', 2023, 'Green', 45999.99),
- ('2C4RC1BG9FR552233', 'Chrysler', 'Pacifica', 2022, 'Purple', 35999.99),
- ('WBA8D9C56HNU10123', 'BMW', '330i', 2021, 'White', 41999.99),
- ('1G1ZD5ST2JF178553', 'Chevrolet', 'Malibu', 2022, 'Yellow', 23999.99);
-       
+INSERT INTO cars (VIN, manufacturer, model, year, color, price) VALUES
+('3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', 2019, 'Blue', 26999.99),
+('ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', 2019, 'Red', 22999.99),
+('RKXVNNIHLVVZOUB4M', 'Ford', 'Fusion', 2018, 'White', 23999.99),
+('HKNDGS7CU31E9Z7JW', 'Toyota', 'RAV4', 2018, 'Silver', 27999.99),
+('DAM41UDN3CHU2WVF6', 'Volvo', 'V60', 2019, 'Gray', 31999.99),
+('DAM41UDN3CHU2WVF6', 'Volvo', 'V60 Cross Country', 2019, 'Gray', 32999.99);
+
 DESCRIBE cars;
+
+INSERT INTO customers (customer_id, name, phone_number, email, address, city, state_province, country, zip_code) VALUES
+(10001, 'Pablo Picasso', '+34 636 17 63 82', NULL, 'Paseo de la Chopera, 14', 'Madrid', 'Madrid', 'Spain', '28045'),
+(20001, 'Abraham Lincoln', '+1 305 907 7086', NULL, '120 SW 8th St', 'Miami', 'Florida', 'United States', '33130'),
+(30001, 'Napoléon Bonaparte', '+33 1 79 75 40 00', NULL, '40 Rue du Colisée', 'Paris', 'Île-de-France', 'France', '75008');
+
+INSERT INTO salespersons (staff_id, name, store) VALUES
+(1, 'Petey Cruiser', 'Madrid'),
+(2, 'Anna Sthesia', 'Barcelona'),
+(3, 'Paul Molive', 'Berlin'),
+(4, 'Gail Forcewind', 'Paris'),
+(5, 'Paige Turner', 'Mimia'),
+(6, 'Bob Frapples', 'Mexico City'),
+(7, 'Walter Melon', 'Amsterdam'),
+(8, 'Shonda Leer', 'São Paulo');
        
+INSERT INTO invoices (invoice_number, date, car_VIN, customer_id, staff_ID, total_price) VALUES
+(852399038, '2018-08-22', '3K096I98581DHSNUP', 10001, 3, 26999.99),
+(731166526, '2018-12-31', 'RKXVNNIHLVVZOUB4M', 30001, 5, 23999.99),
+(271135104, '2019-01-22', 'ZM8G7BEUQZ97IH46V', 20001, 7, 22999.99);
        
+SELECT * FROM cars;
+SELECT * FROM customers;
+SELECT * FROM salespersons;
+SELECT * FROM invoices;
 
